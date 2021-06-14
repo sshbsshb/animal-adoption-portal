d448a21bdd45936efd28a71d31367a828a33bdb0
sshbsshb <sshb@live.com>
2021-06-14 16:09:07 +0800

    Update LoginService.cs

--- AnimalAdoption.Common.Logic/LoginService.cs
+++ AnimalAdoption.Common.Logic/LoginService.cs
@@ -41,7 +41,7 @@ namespace AnimalAdoption.Common.Logic
                     name = "Murphy";
                     break;
                 case "placeholder":
-                    name = "Placeholder";
+                    name = "sagasg";
                     break;
             }
             return name;
